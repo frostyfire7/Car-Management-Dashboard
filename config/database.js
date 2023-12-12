80b15be2f8df30ec4f8a41a586f7fb9603dd3485
juliusadetya <juliusadetya@gmail.com>
2023-12-12 21:44:12 +0700

    edit database connection

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const knex = require("knex");
 
 const database = knex({
   client: "pg",
-  connection: "postgres://postgres:docker@127.0.0.1:5432/postgres", // url postgres
+  connection: "postgres://postgres:JuliusA132435@127.0.0.1:5432/postgres", // url postgres
   searchPath: ["public"], // schema
 });
 
